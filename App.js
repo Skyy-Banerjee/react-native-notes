c87376475da9a20def8f167b9ef5e6c0586beede
Soumadip Banerjee <39thdecember@gmail.com>
2023-10-06 03:13:24 +0530

    FlexBox with React-Native (lessons: 25-38)

--- App.js
+++ App.js
@@ -1,63 +1,26 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet} from 'react-native';
+import Box from './components/Box';
 
 export default function App() {
 	return (
+		//<View style={{backgroundColor: "green", flex:1}}></View>
 		<View style={styles.container}>
-			<View style={styles.darkMode}>
-				<Text style={styles.darkModeText}>Style Inheritance
-				<Text style={styles.boldText}> In Bold</Text>
-				</Text>
-			</View>
-			<View style={[styles.box, styles.lightblueBg, styles.boxShadow]}>
-				<Text style={{ borderRadius: 5, backgroundColor: 'red' }}>
-					Lightblue box{' '}
-				</Text>
-			</View>
-			<View style={[styles.box, styles.lightgreenBg, styles.androidShadow]}>
-				<Text>Lightgreen box</Text>
-			</View>
+			<Box style={{ backgroundColor: '#8e9b00', top:75, left:75 }}>1</Box>
+			<Box style={{ backgroundColor: '#b65d1f' }}>2</Box>
+			<Box style={{ backgroundColor: '#1c4c56' }}>3</Box>
+			<Box style={{ backgroundColor: '#ab9156',position: 'absolute', top:75, left:75}}>4</Box>
+			<Box style={{ backgroundColor: '#6b0803' }}>5</Box>
+			<Box style={{ backgroundColor: '#1c4c',  }}>6</Box>
+			<Box style={{ backgroundColor: '#b95f21' }}>7</Box>
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
-	container: { flex: 1, backgroundColor: 'plum', padding: 60 },
-	darkMode: {
-		backgroundColor: 'black',
-	},
-	darkModeText: {
-		color: 'white',
-	},
-	boldText: {
-		fontWeight: 'bold',
-	},
-	box: {
-		//common style-properties
-		width: 250,
-		height: 250,
-		//padding: 10,
-		paddingHorizontal: 10,
-		paddingVertical: 20,
-		marginVertical: 10,
-		borderWidth: 3,
-		borderColor: 'purple',
-	},
-	lightblueBg: {
-		backgroundColor: 'lightblue',
-	},
-	lightgreenBg: {
-		backgroundColor: 'lightgreen',
-	},
-	boxShadow: {
-		shadowColor: '#333333',
-		shadowOffset: {
-			width: 6,
-			height: 6,
-		},
-		shadowOpacity: 0.6,
-		shadowRadius: 4,
-	},
-	androidShadow: {
-		elevation: 15,
+	container: {
+		flex: 1,
+		marginTop: 64,
+		borderWidth: 6,
+		borderColor: 'red',
 	},
 });
---
+++ components/Box.jsx
@@ -0,0 +1,19 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function Box({ children, style }) {
+	return (
+		<View style={[styles.box, style]}>
+			<Text style={styles.text}>{children}</Text>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	box: { backgroundColor: '#fff', padding: 20, top: 100, left: 100 },
+	text: {
+		fontSize: 24,
+		fontWeight: 'bold',
+		textAlign: 'center',
+		color: 'white',
+	},
+});
